578a22975dd5f1b6b3cd7f3ea2af25a9cf2b2978
kuhn.he <kuhn.he@DESKTOP-K75OFMU>
2018-10-29 13:51:54 +0800

    fix ElasticSimpleJob extends bug.

--- src/main/java/com/dangdang/elasticjob/lite/annotation/ElasticSimpleJob.java
+++ src/main/java/com/dangdang/elasticjob/lite/annotation/ElasticSimpleJob.java
@@ -1,12 +1,14 @@
 package com.dangdang.elasticjob.lite.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.core.annotation.AliasFor;
 
+@Inherited
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ElasticSimpleJob {
